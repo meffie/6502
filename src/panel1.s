29a012540247acf5e45063622fede65f5e6b1931
Michael Meffie <mmeffie@sinenomine.net>
2022-07-24 16:11:35 -0400

    Setup port a data pins

--- src/panel1.s
+++ src/panel1.s
@@ -9,7 +9,7 @@
 ;
 
 ; 65C22 registers
-PA   .equ $6001      ; Output Fegister A
+PA   .equ $6001      ; Output Register A
 DDRA .equ $6003      ; Data Direction Register A
 PCR  .equ $600c      ; Peripheral Control Register
 IFR  .equ $600d      ; Interrupt Flag Register
@@ -21,31 +21,34 @@ INPUT .equ $00
     .org $8000      ; ROM memory mapped location
 
 start:
-    ; Setup read handshake on port A.
+    ; Setup port A data pins.
+    lda #%11110000  ; Set port A pins 0 to 3 to input, 4 to 7 to output
+    sta DDRA
+
+    ; Setup port A read handshake.
     lda #%00001000  ; CA1=negative active edge, CA2=handshake output
     sta PCR
     lda #%10000010  ; Enable interrupt on CA1 activation
     sta IER
+    lda #%01111101  ; Disable all other interrupts
+    sta IER
+
     cli             ; Enable interrupts
 
 spin:               ; main loop
     nop             ;  just hangout and wait for interrupts
-    jmp spin
-
-nmi:                ; Non-maskable interrupt handler
-    rti             ; Done
+    jmp spin        ;  until the end of time
 
 irq:                ; Interrupt handler
-    pha             ; Save A register on the stack
     lda IFR         ; Read interrupt flags
-    nop             ;  todo: check for CA1 flag
-    lda PA          ; Read port A, clear the interrupt
-    and #$0f        ; Only 4 input switches
+    nop             ; todo: check for CA1 flag
+    nop             ; todo: debounce delay
+    lda PA          ; Read port A and clears the interrupt
+    and #$0f        ; We have only 4 input switches
     sta INPUT       ; Store the input nibble
-    pla             ; Restore A register from the stack
     rti             ; Done
 
     .org $fffa      ; Vectors
-    .word nmi       ; Non-maskable Interrupt Request vector
+    .word $0000     ; Non-maskable interrupt is not implemented
     .word start     ; Reset vector
     .word irq       ; Interrupt Request vector
